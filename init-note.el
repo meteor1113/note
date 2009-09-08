a64077dfb222838c1446cafc7a340cb60baf77ad
Meteor Liu <meteor1113@gmail.com>
2009-09-08 08:26:43 +0800

    Signed-off-by: Meteor Liu <meteor1113@gmail.com>

--- init-note.el
+++ init-note.el
@@ -9,32 +9,30 @@
 ;; @date 2009-07-26
 
 
-(require 'org-publish)
-
-
-(let* ((note-dir (file-name-directory (or load-file-name (buffer-file-name))))
-       (parent-dir (directory-file-name
-                    (file-name-directory (directory-file-name note-dir))))
-       (publish-dir (expand-file-name "website" parent-dir)))
-  (setq org-publish-project-alist
-        `(("note-org"
-           :base-directory ,note-dir
-           :publishing-directory ,publish-dir
-           :base-extension "org"
-           :recursive t
-           :publishing-function org-publish-org-to-html
-           :auto-index t
-           :index-filename "index.org"
-           :index-title "index"
-           :link-home "index.html")
-          ("note-static"
-           :base-directory ,note-dir
-           :publishing-directory ,publish-dir
-           :recursive t
-           :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|swf\\|zip\\|gz\\|txt\\|el"
-           :exclude "init-note.el"
-           :publishing-function org-publish-attachment)
-          ("note" :components ("note-org" "note-static")))))
+(when (require 'org-publish nil t)
+  (let* ((ndir (file-name-directory (or load-file-name (buffer-file-name))))
+         (pardir (directory-file-name
+                  (file-name-directory (directory-file-name ndir))))
+         (pdir (expand-file-name "website" pardir)))
+    (setq org-publish-project-alist
+          `(("note-org"
+             :base-directory ,ndir
+             :publishing-directory ,pdir
+             :base-extension "org"
+             :recursive t
+             :publishing-function org-publish-org-to-html
+             :auto-index t
+             :index-filename "index.org"
+             :index-title "index"
+             :link-home "index.html")
+            ("note-static"
+             :base-directory ,ndir
+             :publishing-directory ,pdir
+             :recursive t
+             :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|swf\\|zip\\|gz\\|txt\\|el"
+             :exclude "init-note.el"
+             :publishing-function org-publish-attachment)
+            ("note" :components ("note-org" "note-static"))))))
 
 
 (provide 'init-note)
